924f371345bc8be10f27c913871c8840ef4d6757
Ilya Igonkin <osdev0@gmail.com>
2014-12-23 05:59:56 +0000

    Add open in background feature (Chrome)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,8 +6,15 @@ $(document).ready(function() {
     $.ajax({ url: '/bookmarks/' + id, type: 'delete' })
       .done(function() { bookmark.remove(); })
       .fail(function(jqxhr, status, error) { $.error(error); });
-  }).on('click', '.link', function() {
+  }).on('click', '.link', function(event) {
     var id = $(this).data('id');
+    if (/chrome/i.test(navigator.userAgent)) {
+      var click = document.createEvent('MouseEvents');
+      click.initMouseEvent("click", true, true, window,
+        0, 0, 0, 0, 0, false, false, false, true, 0, null);
+      this.dispatchEvent(click);
+      event.preventDefault();
+    }
     $.ajax({ url: '/bookmarks/' + id + '/hit', type: 'patch' })
       .fail(function(jqxhr, status, error) { $.error(error); });
   });
